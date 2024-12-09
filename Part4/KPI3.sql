8eda2e1b7531bce823497e957668dc5b2cf8d49b
Yodahe Teshome <jodahe2001@gmail.com>
2024-12-09 12:37:19 +0300

    KPI 3 done

---
+++ Part4/KPI3.sql
@@ -0,0 +1,8 @@
+SELECT 
+    (COUNT(DISTINCT g.id) * 100.0 / NULLIF(COUNT(DISTINCT gd.id), 0)) AS conversion_rate
+FROM 
+    public.group_deals gd
+LEFT JOIN 
+    public.groups g ON g.group_deals_id = gd.id
+WHERE 
+    g.status = 'COMPLETED';
