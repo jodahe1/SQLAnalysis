426f3aba55c5b8aaa6ae281238e26394bee823b1
Yodahe Teshome <jodahe2001@gmail.com>
2024-12-10 13:59:20 +0300

    Done

--- Part5/QueryforModelTesting.sql
+++ Part5/QueryforModelTesting.sql
@@ -11,17 +11,17 @@ SELECT
     c.name AS category_name,
     pvs.stock AS current_stock,
     pvp.price AS current_price,
-    COALESCE(SUM(o.total_amount), 0) AS total_sales_last_week,
+    COALESCE(SUM(o.total_amount), 0) AS total_sales_last_3_months,
     COALESCE(AVG(pr.rating), 0) AS average_rating,
-    COUNT(DISTINCT o.id) AS total_orders_last_week,
+    COUNT(DISTINCT o.id) AS total_orders_last_3_months,
     (SELECT COUNT(*) 
      FROM public.orders o2 
-     WHERE o2.created_at >= (SELECT max_date FROM last_order_date) - INTERVAL '30 days' 
-     AND o2.id IN (SELECT pr2.order_id FROM public.product_ratings pr2 WHERE pr2.product_id = p.id)) AS total_orders_last_month,
+     WHERE o2.created_at >= (SELECT max_date FROM last_order_date) - INTERVAL '6 months' 
+     AND o2.id IN (SELECT pr2.order_id FROM public.product_ratings pr2 WHERE pr2.product_id = p.id)) AS total_orders_last_6_months,
     (SELECT COUNT(*) 
      FROM public.orders o3 
-     WHERE o3.created_at >= (SELECT max_date FROM last_order_date) - INTERVAL '7 days' 
-     AND o3.id IN (SELECT pr3.order_id FROM public.product_ratings pr3 WHERE pr3.product_id = p.id)) AS total_orders_last_7_days,
+     WHERE o3.created_at >= (SELECT max_date FROM last_order_date) - INTERVAL '3 months' 
+     AND o3.id IN (SELECT pr3.order_id FROM public.product_ratings pr3 WHERE pr3.product_id = p.id)) AS total_orders_last_3_months,
     pv.weight AS product_weight,
     pv.status AS variation_status
 FROM 
@@ -39,6 +39,6 @@ LEFT JOIN
 LEFT JOIN 
     public.product_ratings pr ON pr.product_id = p.id
 LEFT JOIN 
-    public.orders o ON o.id = pr.order_id AND o.created_at >= (SELECT max_date FROM last_order_date) - INTERVAL '7 days'
+    public.orders o ON o.id = pr.order_id AND o.created_at >= (SELECT max_date FROM last_order_date) - INTERVAL '3 months'
 GROUP BY 
     p.id, pn.name, c.name, pvs.stock, pvp.price, pv.weight, pv.status;
