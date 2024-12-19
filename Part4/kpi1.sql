04bc12d525e62df1575680e006a5f43f1b3c6a55
Yodahe Teshome <jodahe2001@gmail.com>
2024-12-19 09:23:31 +0300

    KPi 1 Dn

--- Part4/kpi1.sql
+++ Part4/kpi1.sql
@@ -1,8 +1,22 @@
+-- Group Deals
 SELECT 
+    SUM(o.total_amount) AS total_revenue,
+    COUNT(*) AS total_deals,
     AVG(o.total_amount) AS average_revenue_per_group_deal
 FROM 
     public.orders o
 JOIN 
     public.groups_carts gc ON o.groups_carts_id = gc.id
 WHERE 
-    gc.status='COMPLETED';  
+    gc.status = 'COMPLETED';
+
+-- Single Deals 
+SELECT 
+    SUM(original_price * quantity) AS net_sales,
+    COUNT(*) AS total_deals
+FROM 
+    public.single_deals
+WHERE 
+    status = 'ACTIVE';
+
+
